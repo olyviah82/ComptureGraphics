9e51323e6b29e01548f985c22bb85ac2da7f50fa
CynthiaGichuki <gichukicynthia1@gmail.com>
2021-11-07 19:33:04 +0300

    Dome

--- OpenGL_Task2/OpenGL_Task2/Source.cpp
+++ OpenGL_Task2/OpenGL_Task2/Source.cpp
@@ -1,8 +1,12 @@
 #include <Windows.h>
 #include <GL\glut.h>
 #include <iostream>
+#include <cmath>
+#define PI 3.14159265359
 
 using namespace std;
+
+
 
 void skyback() {
 	glColor3f(0.5, 0.8, 0.9);
@@ -77,6 +81,16 @@ void house() {
 	glVertex3f(0.1, -0.3, 0);
 	glEnd();
 
+	//window grids 1
+	glColor3f(0.6, 0.2, 0.3);
+	glLineWidth(3);
+	glBegin(GL_LINES);
+	glVertex2d(0.2, -0.1);
+	glVertex2d(0.2, -0.3);
+	glVertex2d(0.1, -0.2);
+	glVertex2d(0.3, -0.2);
+	glEnd();
+
 	//border
 	glColor3f(0, 0, 0);
 	glLineWidth(3.0);
@@ -85,7 +99,7 @@ void house() {
 	glVertex3f(0.3, -0.1, 0);
 	glVertex3f(0.3, -0.3, 0);
 	glVertex3f(0.1, -0.3, 0);
-	
+
 	glEnd();
 
 	//window2
@@ -95,6 +109,16 @@ void house() {
 	glVertex3f(-0.3, -0.1, 0);
 	glVertex3f(-0.3, -0.3, 0);
 	glVertex3f(-0.1, -0.3, 0);
+	glEnd();
+
+	//window grids 2
+	glColor3f(0.6, 0.2, 0.3);
+	glLineWidth(3);
+	glBegin(GL_LINES);
+	glVertex2d(-0.2, -0.1);
+	glVertex2d(-0.2, -0.3);
+	glVertex2d(-0.3, -0.2);
+	glVertex2d(-0.1, -0.2);
 	glEnd();
 
 	//border
@@ -115,6 +139,23 @@ void house() {
 	glVertex3f(0.1, -0.8, 0);
 	glVertex3f(-0.1, -0.8, 0);
 	glEnd();
+
+	//doorknob
+	//int w;
+	//float j = 0.05f; float e = 0.6f; float radius = 0.02f;
+	//int triangleAmount = 40;
+
+	//float twicePi = 2.0f * PI;
+	//glBegin(GL_TRIANGLE_FAN);
+	//glColor3f(0, 0, 0);
+	//glVertex2f(j, e);
+	//for (w = 0; w <= triangleAmount; w++) {
+		//glVertex2f(
+			//j + (radius * cos(w * twicePi / triangleAmount)),
+			//e + (radius * sin(w * twicePi / triangleAmount))
+		//);
+	//}
+	//glEnd();
 
 	//doorborder
 	glColor3f(0, 0, 0);
@@ -151,6 +192,41 @@ void house() {
 		glVertex2f(r * cos(3.14 * p / 180.0) + l, q * sin(3.14 * p / 180.0) + z);
 
 	glEnd();
+
+
+	glColor3f(0.3, 0.5, 0.8);
+	float a = 0.0f;
+	float b = 1.5f;
+	float c = 7.00f;
+	float d = 4.00f;
+	glBegin(GL_TRIANGLE_FAN);
+	for (a = 0.0f; a <= 180; a++)
+		glVertex2f(b * cos(3.14 * a / 180.0) + d, b * sin(3.14 * a / 180.0) + c);
+	glEnd();
+
+
+
+
+
+	int f;
+	float h = 0.0f; float y = 0.3f; float rad = 0.35f;
+	int triangleAm = 40;
+
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(0, 0, 0);
+	glVertex2f(h, y);
+	for (f = 0; f <= triangleAm; f++) {
+		glVertex2f(
+			h + (rad * cos(f * PI / triangleAm)),
+			y + (rad * sin(f * PI / triangleAm))
+		);
+	}
+	glEnd();
+
+	glFlush();
+
+
 
 }
 
